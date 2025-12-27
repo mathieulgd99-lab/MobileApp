d1f61189ceb6fb51f1a706c88ec6688dd0acb33b
Juliette Duclosson <juliette.duclosson@gmail.com>
2025-12-27 04:29:02 +0100

    ntm

---
+++ HelloWorldJSApp/screens/colors.js
@@ -0,0 +1,17 @@
+// colors.js
+
+export const COLORS = {
+  primary: '#F15A24',      // Orange VietClimb
+  primaryDark: '#C9481D',  // Version plus sombre
+  primaryLight: '#FF7A45', // Version plus claire
+
+  white: '#FFFFFF',
+  black: '#000000',
+
+  greyLight: '#F2F2F2',
+  grey: '#BDBDBD',
+  greyDark: '#4F4F4F',
+
+  background: '#1e1e1e',
+  tabInactive: 'gray',
+};
